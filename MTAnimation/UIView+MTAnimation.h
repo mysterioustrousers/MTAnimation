3a60d84fe20d63efb8363ed6d6cd70170e7288ba
Adam Kirk <atomkirk@gmail.com>
2013-06-27 16:04:38 -0600

    making ios only

--- MTAnimation/UIView+MTAnimation.h
+++ MTAnimation/UIView+MTAnimation.h
@@ -6,10 +6,6 @@
 //  Copyright (c) 2013 Mysterious Trousers. All rights reserved.
 //
 
-#if TARGET_IPHONE_OS
-#import <UIKit/UIKit.h>
-#endif
-
 #import "MTTimingFunctions.h"
 #import "MTAnimationTypes.h"
 
